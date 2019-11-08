d42883859680747fc0275d11be6e29d2653c3c8d
Andrew Lorber <andrewlorber@aol.com>
2019-11-08 12:02:13 -0500

    Adds game setup

--- main.cpp
+++ main.cpp
@@ -7,10 +7,15 @@
 //
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int currentBoard[6][7] = {0};
+
+void initUserBoard(){
+    
+}
 
 void printBoard(){
     cout << " |-1-|-2-|-3-|-4-|-5-|-6-|-7-|\n\n";
@@ -25,9 +30,72 @@ void printBoard(){
     }
 }
 
+bool player1Starts(bool userPlaying){
+    string resp;
+    if(userPlaying){
+        cout << "Would you like to go first? (Y/N) \n";
+    } else {
+        cout << "Would you like player 1 to go first? (Y/N) \n";
+    }
+    
+    cin >> resp;
+    while(resp.compare("Y") != 0 && resp.compare("N") != 0){
+        cout << "That was an invalid response. Please respond with 'Y' or 'N'." << '\n';
+        cin >> resp;
+    }
+    if(resp.compare("Y") == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
 
-
-
+void playGame(){
+    
+    // User decides game board
+    double input;
+    cout << "Welcome to checkers!\nPlease respond with the number of your choice:\n";
+    cout << "1. Begin a new game\n2. Load a custom board\n";
+    cin >> input;
+    while(input != 1 && input != 2){
+        cout << "That was an invalid choice. Please respond with 1 or 2.\n";
+        cin >> input;
+    }
+    if(input == 2){
+        initUserBoard();
+    }
+    
+    // User Vs AI or AI Vs AI
+    bool AIvsAI = false;
+    cout << "Please choose a game mode:\n";
+    cout << "1. User Vs AI\n2. AI Vs AI\n";
+    cin >> input;
+    while(input != 1 && input != 2){
+        cout << "That was an invalid choice. Please respond with 1 or 2.\n";
+        cin >> input;
+    }
+    AIvsAI = (input == 2);
+    
+    // User inputs time limit
+    cout << "Please enter the number of seconds (integer value) that the AI has to move\n";
+    cin >> input;
+    while(floor(input) <= 1){
+        cout << "Please enter an integer value greater than 1\n";
+        cin >> input;
+    }
+    
+    // Decides starting player
+    int startingPlayer;
+    if(player1Starts(!AIvsAI)){
+        startingPlayer = 1;
+    } else {
+        startingPlayer = 2;
+    }
+    cout << "The starting player is player " << startingPlayer << ".\n\n";
+    
+    
+    
+}
 
 int main(int argc, const char * argv[]) {
     printBoard();
